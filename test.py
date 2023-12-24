eff61c7596a73e58f415ede8de568302573e4946
TinnyTomato <1872401992@qq.com>
2023-12-24 15:34:41 +0800

    sonarcloud_test4zzy

---
+++ test.py
@@ -0,0 +1,22 @@
+import numpy as np
+import panadas as pd
+
+def sonarcloud_test4zzy(t):  
+    a = 10
+    b = 5
+    c = 5
+    d = 20
+    t = d
+
+    if a != b:
+        print("True")
+    if b == c:
+        print("True")
+    if (a + b + c) == d:
+        print("True")
+
+
+t = 100
+sonarcloud_test4zzy(t)
+
+
